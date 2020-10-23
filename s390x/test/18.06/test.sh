1069eed7987ad087838cf6052bedc8363cc85415
charles-korpics <charles.korpics@ibm.com>
2020-10-23 00:09:14 -0400

    testing

--- s390x/test/18.06/test.sh
+++ s390x/test/18.06/test.sh
@@ -9,7 +9,7 @@ export ANSI_RESET="\e[0m"
 echo -e "\n $ANSI_YELLOW *** FUNCTIONAL TEST(S) *** $ANSI_RESET \n"
 
 echo -e "$ANSI_YELLOW Docker is running - docker.sock is mounted with read only mode: $ANSI_RESET"
-docker build -t test/run-app/quay.io/ibmz/docker .
+docker build -t test/run-app/quay.io/ibmz/docker docker
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock:ro test/run-app/quay.io/ibmz/docker version
 docker rmi test/run-app/quay.io/ibmz/docker
 
