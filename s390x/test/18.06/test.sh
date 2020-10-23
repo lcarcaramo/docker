0ce33dfa43ee6d446e1af9f59444c239b438c70f
korpx-z <72992136+korpx-z@users.noreply.github.com>
2020-10-23 00:04:47 -0400

    Update test.sh

--- s390x/test/18.06/test.sh
+++ s390x/test/18.06/test.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo "No tests yet."
+set -e
+
+export ANSI_YELLOW="\e[1;33m"
+export ANSI_GREEN="\e[32m"
+export ANSI_RESET="\e[0m"
+
+echo -e "\n $ANSI_YELLOW *** FUNCTIONAL TEST(S) *** $ANSI_RESET \n"
+
+echo -e "$ANSI_YELLOW Docker is running - docker.sock is mounted with read only mode: $ANSI_RESET"
+docker build -t test/run-app/quay.io/ibmz/docker .
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock:ro test/run-app/quay.io/ibmz/docker version
+docker rmi test/run-app/quay.io/ibmz/docker
+
+echo -e "\n $ANSI_GREEN *** FUNCTIONAL TEST(S) COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
